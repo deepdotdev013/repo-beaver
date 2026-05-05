9b3f690cfb368e11adccbc15c3663452fbb1b488
deep <deepdotdev013@gmail.com>
2026-05-05 23:26:40 +0530

    fix(version): update version number to 0.1.13 and correct archive file reference

--- cmd/root.go
+++ cmd/root.go
@@ -6,7 +6,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var version = "0.1.12"
+var version = "0.1.13"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
